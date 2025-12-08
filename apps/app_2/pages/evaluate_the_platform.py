be8e3d3764fb7c03fec77b250d9e76ee880abe77
Ntetack <98791406+Ntetack@users.noreply.github.com>
2025-12-08 17:01:07 +0000

    Update evaluate_the_platform.py

--- apps/app_2/pages/evaluate_the_platform.py
+++ apps/app_2/pages/evaluate_the_platform.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 # load css
-css_file = Path(__file__).parent / "styles" / "template1_style.css"
+css_file = "/mount/src/streamlit_scrap_data_app/apps/app_2/styles/template1_style.css"
 
 with open(css_file) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
