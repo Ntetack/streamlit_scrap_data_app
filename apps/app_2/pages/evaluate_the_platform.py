634db4507f4154cc9bc70425ddb55ff89efa31fb
Ntetack <98791406+Ntetack@users.noreply.github.com>
2025-12-08 16:59:03 +0000

    Update evaluate_the_platform.py

--- apps/app_2/pages/evaluate_the_platform.py
+++ apps/app_2/pages/evaluate_the_platform.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 import os
+from pathlib import Path
 
-css_file = "styles/template1_style.css" 
+# load css
+css_file = Path(__file__).parent / "styles" / "template1_style.css"
+
 with open(css_file) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
