43ff8035b95874f9ae33b483fb870ae72ebaeca5
Ntetack <98791406+Ntetack@users.noreply.github.com>
2025-12-08 16:34:59 +0000

    Update home.py

--- apps/app_2/home.py
+++ apps/app_2/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 
 # load css
-css_path = Path(__file__).parent / "styles" / "template1_style.css"
+css_file = Path(__file__).parent / "styles" / "template1_style.css"
 with open(css_file) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
