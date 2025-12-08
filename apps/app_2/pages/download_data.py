9caf53f09b2b8ed748f905f59f5d6aa2c9becffe
Ntetack <98791406+Ntetack@users.noreply.github.com>
2025-12-08 16:58:15 +0000

    Update download_data.py

--- apps/app_2/pages/download_data.py
+++ apps/app_2/pages/download_data.py
@@ -6,7 +6,6 @@ from pathlib import Path
 # load css
 css_file = Path(__file__).parent / "styles" / "template1_style.css"
 
-
 with open(css_file) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
